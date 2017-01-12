dd9233c14e0409a6d584a4210934b367747bf9c6
Garrick McCaskill <kiwilifter808@gmail.com>
2017-01-12 22:54:53 +1300

    Add weather app class weather-data and api request service.

---
+++ dev/weather/weather.service.ts
@@ -0,0 +1,10 @@
+import {Injectable} from "angular2/core";
+import {WEATHER_ITEMS} from "./weather.data";
+
+@Injectable()
+
+export class WeatherService {
+	getWeatherItems(){
+		return WEATHER_ITEMS;
+	}
+}
