933ebfa9e567f9f4b7f8c18921f9f1bfd104d62f
Garrick McCaskill <kiwilifter808@gmail.com>
2017-01-12 22:51:43 +1300

    Add weather app components item and list.

--- dev/weather/weather-list.component.ts
+++ dev/weather/weather-list.component.ts
@@ -1,16 +1,26 @@
 import {Component} from "angular2/core";
 import {WeatherItemComponent} from "./weather-item.component";
+import {WeatherItem} from "./weather-item";
+import {WeatherService} from "./weather.service";
 
 @Component({
 	selector: 'weather-list',
 	template: `
 		<section class="weather-list">
-			<weather-item></weather-item>
+			<weather-item *ngFor="#weatherItem of weatherItems" [item]="weatherItem"></weather-item>
 		</section>
 	`,
-	directives: [WeatherItemComponent]
+	directives: [WeatherItemComponent],
+	providers: [WeatherService]
 })
 
-export class WeatherListComponent {
-
+export class WeatherListComponent implements OnInit{
+	weatherItems: WeatherItem[];
+	
+	constructor(private _weatherService: WeatherService){}
+	
+	ngOnInit():any {
+		this.weatherItems = this._weatherService.getWeatherItems();
+	}
+	
 }
