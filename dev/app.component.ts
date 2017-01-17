810e405f5e7dc9f159291b19da0d2e5ebd572458
Garrick McCaskill <kiwilifter808@gmail.com>
2017-01-18 08:12:19 +1300

    Add side-bar component to app, add delete functionality to profiles.

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,5 +1,7 @@
 import {Component} from 'angular2/core';
+import {SidebarComponent} from "./sidebar.component";
 import {WeatherListComponent} from "./weather/weather-list.component";
+import {WeatherSearchComponent} from "./weather/weather-search.component";
 
 @Component({
     selector: 'my-app',
@@ -7,9 +9,12 @@ import {WeatherListComponent} from "./weather/weather-list.component";
 		<header>
 			<h1>Angular 2 Weather</h1>
 		</header>
+		<my-sidebar></my-sidebar>
+		<my-weather-search></my-weather-search>
 		<weather-list></weather-list>
     `,
-	directives: [WeatherListComponent]
+	directives: [WeatherListComponent, WeatherSearchComponent, SidebarComponent],
+	styleUrls: ['src/css/app.css']
 	
 })
 
