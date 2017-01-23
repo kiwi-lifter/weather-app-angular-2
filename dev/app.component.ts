d1b2b0c4ba845a3fb65d1db5a8bb09d71fe5c193
Garrick McCaskill <kiwilifter808@gmail.com>
2017-01-23 22:56:14 +1300

    Add bootstrap container, row and col classes and divs.

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -6,12 +6,22 @@ import {WeatherSearchComponent} from "./weather/weather-search.component";
 @Component({
     selector: 'my-app',
     template: `
+	<div class="row">
 		<header>
 			<h1>Angular 2 Weather</h1>
 		</header>
-		<my-sidebar></my-sidebar>
-		<my-weather-search></my-weather-search>
-		<weather-list></weather-list>
+	</div>
+	<div class="row">
+		<div class="col">
+			<my-sidebar></my-sidebar>
+		</div>
+		<div class="col">
+			<my-weather-search></my-weather-search>
+		</div>
+		<div class="col">
+			<weather-list></weather-list>
+		</div>
+	</div>	
     `,
 	directives: [WeatherListComponent, WeatherSearchComponent, SidebarComponent],
 	styleUrls: ['src/css/app.css']
